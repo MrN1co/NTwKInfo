c887985d9578416ca6667c238504788820e6bafe
kgrabias <kgrabias@student.agh.edu.pl>
2025-11-19 02:05:19 +0100

    poprawa autorefresh

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,4 @@
-// Funkcja rekurencyjna do automatycznego odświeżania strony co 60 sekund ~ KG
-function autoRefresh() {
-    setTimeout(function() {
-        location.reload();
-    }, 60000); // w milisekundach
-    autoRefresh();
-}
-
-// Uruchomienie auto-odświeżania po załadowaniu strony ~ KG
-window.onload = autoRefresh;
-console.log('JavaScript załadowany');//debug
+// Automatyczne odświeżanie strony co 60 sekund ~ KG
+setTimeout(function() {
+    location.reload();
+}, 60000); // 60 sekund w milisekundach
